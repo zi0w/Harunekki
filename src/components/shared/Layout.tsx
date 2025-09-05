43e11e52b1836bd9d0db567d0b7d10014243f951
zi0w <zzz13588@naver.com>
2025-09-05 09:50:26 +0900

    feat: 마이페이지 구현 완료(뱃지 페이지 제외)

--- src/components/shared/Layout.tsx
+++ src/components/shared/Layout.tsx
@@ -6,26 +6,22 @@ import { Outlet, useLocation } from 'react-router-dom';
 const Layout = () => {
   const { pathname } = useLocation();
 
-  // const hideHeader =
-  //   pathname === '/login' || pathname.startsWith('/auth') || pathname.startsWith('/mypage/info');
-
   const isAuth = pathname === '/login' || pathname.startsWith('/auth');
   const isOnboarding = pathname === '/onboarding';
   const isInfo = pathname.startsWith('/mypage/info');
   const isInfoEdit = pathname === '/mypage/info/edit';
+  const isBadge = pathname.startsWith('/mypage/badges'); // ← 앞에 슬래시 빠졌던 거도 고쳐줌
 
   const hideFooter =
     pathname === '/login' ||
     pathname === '/onboarding' ||
-    pathname.startsWith('/mypage/info');
+    pathname.startsWith('/mypage/info') ||
+    pathname.startsWith('/mypage/badges');
 
   return (
     <div className="relative w-[375px] h-[812px] bg-[#F9FAFB] overflow-hidden">
       <div className="flex h-full flex-col">
-        {/* 헤더 자리: 높이 고정 */}
-        {/* <header className="h-[94px] shrink-0">
-          {!hideHeader && <Header />}
-        </header> */}
+        {/* 헤더 */}
         {!isAuth &&
           (isInfo ? (
             <HeaderBar
@@ -34,17 +30,25 @@ const Layout = () => {
               backTo={isInfoEdit ? '/mypage/info' : '/mypage'}
               heightClassName="h-[94px]"
               bgClassName="bg-[#F9FAFB]"
-              withBorder={false}
+            />
+          ) : isBadge ? (
+            <HeaderBar
+              variant="back"
+              title="내가 받은 뱃지"
+              backTo="/mypage"
+              heightClassName="h-[94px]"
+              bgClassName="bg-[#F9FAFB]"
             />
           ) : (
             <Header />
           ))}
-        {/* 콘텐츠: 남는 공간 전부 차지, 스크롤 가능 */}
+
+        {/* 콘텐츠 */}
         <main className="flex-1 mx-5">
           <Outlet />
         </main>
 
-        {/* 하단 네비 자리: 높이 고정 */}
+        {/* 하단 네비 */}
         <footer className="h-[102px] shrink-0 bg-[#FDFDFE]">
           {!hideFooter && <BottomNav />}
         </footer>
