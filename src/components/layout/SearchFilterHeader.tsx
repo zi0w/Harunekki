309de873998343d4f40e29a1ac4b0624673fa4f9
Ain1204 <emily030528@gmail.com>
2025-09-18 12:08:14 +0900

    refactor:button오류수정

--- src/components/layout/SearchFilterHeader.tsx
+++ src/components/layout/SearchFilterHeader.tsx
@@ -44,7 +44,7 @@ export default function SearchFilterHeader({
             {/* 🔍 버튼화 */}
             <button
               onClick={handleSearch}
-              className="p-0 w-5 h-5 flex items-center justify-center bg-[#F0F0F0] "
+              className="p-0 w-5 h-5 flex items-center justify-center bg-[#F0F0F0] appearance-none border-none outline-none"
             >
               <img src={SearchIcon} className="w-full h-full" alt="검색" />
             </button>
@@ -68,9 +68,9 @@ export default function SearchFilterHeader({
             onClick={() => {
               navigate(isFilterPage ? basePath : `${basePath}/filter`);
             }}
-            className="shrink-0 ml-2 bg-[#F9FAFB]"
+            className="shrink-0 ml-2 bg-[#F9FAFB] appearance-none border-none outline-none"
           >
-            <img src={FilterIcon} className="w-6 h-6" />
+            <img src={FilterIcon} className="w-6 h-6" alt="필터" />
           </button>
         </div>
       }
